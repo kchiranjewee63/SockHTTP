a4f7bd322bc7f1c8722c1ceadf2ff91bf5cdf44c
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-03-29 23:25:02 -0400

    Add backlog parameter in the Server class

--- httpServer.py
+++ httpServer.py
@@ -6,14 +6,15 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 class Server:
-    def __init__(self, handler, thread_pool_size = 4):
+    def __init__(self, handler, backlog = 5, thread_pool_size = 4):
         self.handler = handler
+        self.backlog = backlog
         self.thread_pool_size = thread_pool_size
 
     def start(self, port):
         server_socket = socket.socket()
         server_socket.bind(('0.0.0.0', port))
-        server_socket.listen(5)
+        server_socket.listen(self.backlog)
         logging.info(f'Listening on port {port}...')
         executor = ThreadPoolExecutor(max_workers = self.thread_pool_size)
         while True:
