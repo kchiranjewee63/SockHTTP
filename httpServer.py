3eabcff0f3dbb40d72d2afc403620d1056500254
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-03-29 23:23:59 -0400

    Add Server class

---
+++ httpServer.py
@@ -0,0 +1,22 @@
+import socket
+from concurrent.futures import ThreadPoolExecutor
+from processing import threaded
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+class Server:
+    def __init__(self, handler, thread_pool_size = 4):
+        self.handler = handler
+        self.thread_pool_size = thread_pool_size
+
+    def start(self, port):
+        server_socket = socket.socket()
+        server_socket.bind(('0.0.0.0', port))
+        server_socket.listen(5)
+        logging.info(f'Listening on port {port}...')
+        executor = ThreadPoolExecutor(max_workers = self.thread_pool_size)
+        while True:
+            client_socket, address = server_socket.accept()
+            logging.info(f"Connected to {address[0]}:{address[1]}")
+            executor.submit(threaded, self.handler, client_socket, address)
