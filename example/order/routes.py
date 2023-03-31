0203210072a16e9ef01f74a59033e8798dcc46f1
Chiranjeewee Koirala <kchiranjewee63@gmail.com>
2023-03-31 11:02:34 -0400

    Add order service routes and server

---
+++ example/order/routes.py
@@ -0,0 +1,36 @@
+from SockHTTP import response
+import json
+import requests
+import logging
+from readerwriterlock import rwlock
+import os
+
+logging.basicConfig(level = logging.DEBUG)
+
+catalog_host = os.environ.get('CATALOG_HOST')
+catalog_port = os.environ.get('CATALOG_PORT')
+
+order_rwlock = rwlock.RWLockFair()
+
+transaction_number = 0
+
+def orderHandler(request):
+    request_body = json.loads(request.body)
+    catalog_response = requests.post(f"http://{catalog_host}:{catalog_port}/catalog/orders", json = request_body)
+    catalog_http_code = catalog_response.status_code
+    catalog_headers = catalog_response.headers
+    catalog_body = catalog_response.text
+    catalog_response.close()
+    if catalog_http_code == 200:
+        with order_rwlock.gen_wlock():
+            global transaction_number
+            transaction_number += 1
+            body = {
+                "data": {
+                    "transaction_number": transaction_number
+                }
+            }
+            return response.Response(200, json.dumps(body), {"Content-Type": "application/json"})
+    else:
+        logging.error(f"Received a {catalog_http_code} error from the catalog service: {catalog_body}")
+        return response.Response(catalog_http_code, catalog_body, {"Content-Type": catalog_headers["Content-Type"]})
